e66848b3f9b74c9ed1fd79a6431cb34876731082
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2025-02-03 22:34:28 +0000

    Add flat-type support and refactor shared memory implementation

--- include/image-shm-dblbuf/flat_shm_producer_consumer.hpp
+++ include/image-shm-dblbuf/flat_shm_producer_consumer.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "flat_shared_memory.hpp"
-#include "semaphore_impl.h"
+#include "impl/flat_shm.h"
+#include "impl/semaphore.h"
 #include <functional>
 
 namespace flat_shm
@@ -11,18 +11,18 @@ namespace flat_shm
         static constexpr std::expected<FlatShmProducerConsumer<T>, std::string> create(std::string const &shm_name) noexcept
         {
             auto const size = sizeof(T);
-            auto impl = flat_shm_impl::create(shm_name, size);
+            auto impl = shm::impl::create(shm_name, size);
             if (impl.has_value())
             {
                 auto const sem_read_name = fmt::format("/{}_read", shm_name);
-                auto sem_read = flat_shm_impl::create(sem_read_name, 0);
+                auto sem_read = shm::impl::create(sem_read_name, 0);
                 if (!sem_read.has_value())
                 {
                     return std::unexpected(std::move(sem_read.error()));
                 }
 
                 auto const sem_write_name = fmt::format("/{}_write", shm_name);
-                auto sem_write = flat_shm_impl::create(sem_write_name, 1);
+                auto sem_write = shm::impl::create(sem_write_name, 1);
                 if (!sem_write.has_value())
                 {
                     return std::unexpected(std::move(sem_write.error()));
@@ -48,7 +48,7 @@ namespace flat_shm
         {
             if (this != &other)
             {
-                flat_shm_impl::destroy(impl_);
+                shm::impl::destroy(impl_);
                 impl_ = std::move(other.impl_);
                 sem_read_ = std::move(other.sem_read_);
                 sem_write_ = std::move(other.sem_write_);
@@ -58,51 +58,51 @@ namespace flat_shm
 
         ~FlatShmProducerConsumer() noexcept
         {
-            flat_shm_impl::destroy(impl_);
-            flat_shm_impl::destroy(sem_read_);
-            flat_shm_impl::destroy(sem_write_);
+            shm::impl::destroy(impl_);
+            shm::impl::destroy(sem_read_);
+            shm::impl::destroy(sem_write_);
         }
 
         inline void produce(T const &data) noexcept
         {
-            flat_shm_impl::wait(sem_write_);
+            shm::impl::wait(sem_write_);
             write_ref() = data;
-            flat_shm_impl::post(sem_read_);
+            shm::impl::post(sem_read_);
         }
 
         inline T const &consume() noexcept
         {
-            flat_shm_impl::wait(sem_read_);
+            shm::impl::wait(sem_read_);
             T const &data = read();
-            flat_shm_impl::post(sem_write_);
+            shm::impl::post(sem_write_);
             return data;
         }
 
         void consume(std::function<void(T const &)> consumer) noexcept
         {
-            flat_shm_impl::wait(sem_read_);
+            shm::impl::wait(sem_read_);
             consumer(read());
-            flat_shm_impl::post(sem_write_);
+            shm::impl::post(sem_write_);
         }
 
     private:
-        flat_shm_impl::shm impl_;
-        flat_shm_impl::Semaphore sem_read_;
-        flat_shm_impl::Semaphore sem_write_;
+        shm::impl::shm impl_;
+        shm::impl::Semaphore sem_read_;
+        shm::impl::Semaphore sem_write_;
 
-        constexpr FlatShmProducerConsumer(flat_shm_impl::shm &&impl, flat_shm_impl::Semaphore &&sem_read, flat_shm_impl::Semaphore &&sem_write) noexcept
+        constexpr FlatShmProducerConsumer(shm::impl::shm &&impl, shm::impl::Semaphore &&sem_read, shm::impl::Semaphore &&sem_write) noexcept
             : impl_{std::move(impl)}, sem_read_{std::move(sem_read)}, sem_write_{std::move(sem_write)}
         {
         }
 
         inline T &write_ref() noexcept
         {
-            return *static_cast<T *>(flat_shm_impl::write_ref_unsafe(impl_));
+            return *static_cast<T *>(shm::impl::write_ref_unsafe(impl_));
         }
 
         inline T const &read() const noexcept
         {
-            return *static_cast<T const *>(flat_shm_impl::read_unsafe(impl_));
+            return *static_cast<T const *>(shm::impl::read_unsafe(impl_));
         }
     };
 } // namespace flat_shm
