7e0d97db7bb122fefa8adfc1eaa4583650c3b029
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2025-02-09 19:30:24 +0200

    Add error handling and refactor DoubleBufferShem for improved memory management

--- include/image-shm-dblbuf/flat_shm_producer_consumer.hpp
+++ include/image-shm-dblbuf/flat_shm_producer_consumer.hpp
@@ -8,81 +8,30 @@ namespace flat_shm
     template <typename T>
     struct FlatShmProducerConsumer
     {
-        static constexpr std::expected<FlatShmProducerConsumer<T>, std::string> create(std::string const &shm_name) noexcept
+        constexpr FlatShmProducerConsumer(std::string const &shm_name)
+            : impl_(shm_name, sizeof(T)),
+              sem_read_(shm_name + "_read", 0),
+              sem_write_(shm_name + "_write", 1)
         {
-            auto const size = sizeof(T);
-            auto impl = shm::impl::create(shm_name, size);
-            if (impl.has_value())
-            {
-                auto const sem_read_name = fmt::format("/{}_read", shm_name);
-                auto sem_read = shm::impl::create(sem_read_name, 0);
-                if (!sem_read.has_value())
-                {
-                    return std::unexpected(std::move(sem_read.error()));
-                }
-
-                auto const sem_write_name = fmt::format("/{}_write", shm_name);
-                auto sem_write = shm::impl::create(sem_write_name, 1);
-                if (!sem_write.has_value())
-                {
-                    return std::unexpected(std::move(sem_write.error()));
-                }
-
-                return FlatShmProducerConsumer<T>{
-                    std::move(impl.value()),
-                    std::move(sem_read.value()),
-                    std::move(sem_write.value())};
-            }
-            return std::unexpected(impl.error());
         }
 
-        FlatShmProducerConsumer(FlatShmProducerConsumer const &) = delete;
-        FlatShmProducerConsumer &operator=(FlatShmProducerConsumer const &) = delete;
-        FlatShmProducerConsumer(FlatShmProducerConsumer &&other)
+        inline void produce(T const &data)
         {
-            impl_ = std::move(other.impl_);
-            sem_read_ = std::move(other.sem_read_);
-            sem_write_ = std::move(other.sem_write_);
-        }
-        FlatShmProducerConsumer &operator=(FlatShmProducerConsumer &&other)
-        {
-            if (this != &other)
-            {
-                shm::impl::destroy(impl_);
-                impl_ = std::move(other.impl_);
-                sem_read_ = std::move(other.sem_read_);
-                sem_write_ = std::move(other.sem_write_);
-            }
-            return *this;
-        }
-
-        ~FlatShmProducerConsumer() noexcept
-        {
-            shm::impl::destroy(impl_);
-            shm::impl::destroy(sem_read_);
-            shm::impl::destroy(sem_write_);
-        }
-
-        inline void produce(T const &data) noexcept
-        {
-            shm::impl::wait(sem_write_);
+            sem_write_.wait();
             get() = data;
-            shm::impl::post(sem_read_);
+            sem_read_.post();
         }
 
-        inline T const &consume() noexcept
+        inline T const &consume_unsafe()
         {
-            shm::impl::wait(sem_read_);
-            T const &data = get();
-            shm::impl::post(sem_write_);
-            return data;
+            return get();
         }
 
         void consume(std::function<void(T const &)> consumer) noexcept
         {
-            shm::impl::wait(sem_read_);
+            sem_read_.wait();
             consumer(get());
-            shm::impl::post(sem_write_);
+            sem_write_.post();
         }
 
     private:
@@ -90,14 +39,9 @@ namespace flat_shm
         shm::impl::Semaphore sem_read_;
         shm::impl::Semaphore sem_write_;
 
-        constexpr FlatShmProducerConsumer(shm::impl::Shm &&impl, shm::impl::Semaphore &&sem_read, shm::impl::Semaphore &&sem_write) noexcept
-            : impl_{std::move(impl)}, sem_read_{std::move(sem_read)}, sem_write_{std::move(sem_write)}
-        {
-        }
-
         inline T &get() noexcept
         {
-            return *static_cast<T *>(shm::impl::get(impl_));
+            return *static_cast<T *>(impl_.get());
         }
 
     };
--- src/flat_shm_img_producer_consumer_nb.cpp
+++ src/flat_shm_img_producer_consumer_nb.cpp
@@ -10,34 +10,13 @@ using namespace nb::literals;
 struct ProducerConsumer
 {
     shm::impl::Shm shm_;
-    shm::impl::Semaphore sem_read_;
-    shm::impl::Semaphore sem_write_;
     std::shared_ptr<img::Image4K_RGB> image_ = std::make_shared<img::Image4K_RGB>();
+
+    ProducerConsumer(std::string const &shm_name)
+        : shm_(shm_name, sizeof(img::Image4K_RGB))
+    {
+    }
 };
-
-[[nodiscard]] constexpr ProducerConsumer create(const std::string &shm_name)
-{
-    auto impl = shm::impl::create(shm_name, sizeof(img::Image4K_RGB));
-    if (!impl)
-        throw std::runtime_error(impl.error());
-
-    auto sem_read = shm::impl::create(shm_name + "_read", 0);
-    if (!sem_read)
-        throw std::runtime_error(sem_read.error());
-
-    auto sem_write = shm::impl::create(shm_name + "_write", 1);
-    if (!sem_write)
-        throw std::runtime_error(sem_write.error());
-
-    return {std::move(impl.value()), std::move(sem_read.value()), std::move(sem_write.value())};
-}
-
-void destroy(ProducerConsumer &producer_consumer)
-{
-    shm::impl::destroy(producer_consumer.shm_);
-    shm::impl::destroy(producer_consumer.sem_read_);
-    shm::impl::destroy(producer_consumer.sem_write_);
-}
 
 //--------------------------------------------------------------------------------------------
 
@@ -63,77 +42,38 @@ NB_MODULE(Share_memory_image_producer_consumer_nb, m)
     nb::class_<ReturnImage>(m, "ReturnImage")
         .def(nb::init<>())
         .def("timestamp", [](const ReturnImage &self)
-             {
-        if (!self.img_ptr_)
-        {
-            throw std::runtime_error("Image pointer is null.");
-        }
-        return (*self.img_ptr_)->timestamp; })
+             { return (*self.img_ptr_)->timestamp; })
 
         .def("frame_number", [](const ReturnImage &self)
-             {
-        if (!self.img_ptr_)
-        {
-            throw std::runtime_error("Image pointer is null.");
-        }
-        return (*self.img_ptr_)->frame_number; })
+             { return (*self.img_ptr_)->frame_number; })
 
         .def("get_data", [](const ReturnImage &self)
-             { if (!self.img_ptr_)
-             {
-                 throw std::runtime_error("Image pointer is null.");
-             }
-                return nb::ndarray<uint8_t const, nb::numpy, nb::shape<2160, 3840, 3>>(((*self.img_ptr_)->data.data())); }, nb::rv_policy::reference_internal)
+             { return nb::ndarray<uint8_t const, nb::numpy, nb::shape<2160, 3840, 3>>(((*self.img_ptr_)->data.data())); }, nb::rv_policy::reference_internal)
         .def("__repr__", [](const ReturnImage &self) -> std::string
-             {
-        if (!self.img_ptr_)
-        {
-            throw std::runtime_error("Image pointer is null.");
-        }
-         return fmt::format("ReturnImage(ptr = {:p}, timestamp = {}, frame_number = {})",
-                           static_cast<const void*>(*self.img_ptr_),
-                           (*self.img_ptr_)->timestamp,
-                           (*self.img_ptr_)->frame_number); });
+             { return fmt::format("ReturnImage(ptr = {:p}, timestamp = {}, frame_number = {})",
+                                  static_cast<const void *>(*self.img_ptr_),
+                                  (*self.img_ptr_)->timestamp,
+                                  (*self.img_ptr_)->frame_number); });
 
     nb::class_<ProducerConsumer>(m, "ProducerConsumer")
-        .def_static("create", [](nb::str const &shm_name)
-                    { return create(shm_name.c_str()); })
-        .def(nb::init<>())
-        .def("close", &destroy)
+        .def(nb::init<std::string>(), nb::rv_policy::reference_internal)
         .def("store", [](ProducerConsumer &self, img::Image4K_RGB const &image)
-             {
-            if (!self.image_) throw std::runtime_error("Image pointer is null.");
-            std::memcpy(self.shm_.data_, &image, sizeof(img::Image4K_RGB));
-            shm::impl::post(self.sem_read_); })
+             { *static_cast<img::Image4K_RGB *>(self.shm_.get()) = image; })
         .def("load", [](ProducerConsumer &self) -> std::shared_ptr<img::Image4K_RGB>
              {
-            if (!self.image_) throw std::runtime_error("Image pointer is null.");
-            shm::impl::wait(self.sem_read_);
-            std::memcpy(self.image_.get(), self.shm_.data_, sizeof(img::Image4K_RGB));
-            return self.image_; }, nb::rv_policy::reference_internal);
+                 *self.image_.get() = *static_cast<img::Image4K_RGB *>(self.shm_.get());
+                 return self.image_; }, nb::rv_policy::reference_internal);
 
     nb::class_<DoubleBufferShem>(m, "DoubleBufferShem")
-        .def_static("create", [](nb::str const &shm_name)
-                    { return create_shm(shm_name.c_str()); })
-        .def(nb::init<shm::impl::Shm &&, shm::impl::Semaphore &&>())
-        .def("close", &destroy_shm)
+        .def(nb::init<std::string, std::size_t>(), nb::rv_policy::reference_internal)
         .def("store", [](DoubleBufferShem &self, img::Image4K_RGB const &image)
-             {
-                 if (!self.pre_allocated_)
-                     throw std::runtime_error("Image pointer is null.");
-                 store(self, image);
-             })
+             { self.store(image); })
 
         .def("load", [](DoubleBufferShem &self) -> ReturnImage
-             {
-                 if (!self.pre_allocated_)
-                     throw std::runtime_error("Image pointer is null.");
-                 return load(self);
-             },
-             nb::rv_policy::reference_internal)
-        .def("__repr__", [](const DoubleBufferShem &self) -> std::string
+             { return self.load(); }, nb::rv_policy::reference_internal)
+        .def("__repr__", [](DoubleBufferShem const &self) -> std::string
              { return fmt::format("DoubleBufferShem(shm = {:p}, img_ptr = {:p}, img = {:p})",
-                                  static_cast<const void *>(self.shm_.data_),
+                                  self.shm_.get(),
                                   static_cast<const void *>(self.img_ptr_),
                                   static_cast<const void *>(self.pre_allocated_.get())); });
 }
